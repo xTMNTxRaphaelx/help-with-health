b50bb263a27cbec3f2b4e9884c162051ac53f6ed
Rahul Garg <rahul_garg08@yahoo.com>
2020-05-17 23:46:14 +0530

    remove waste code

--- app/components/HandWash.js
+++ app/components/HandWash.js
@@ -3,10 +3,8 @@ import { Platform, View, Text, Button } from "react-native";
 
 class HandWash extends React.Component {
   render() {
-    const { test } = this.props
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>{test}</Text>
         <Text> Component Called</Text>
       </View>
     );
